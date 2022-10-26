f55e64caa332a3b5ea6b21107db255c4c62536a9
khanian <khanylee@gmail.com>
2022-10-26 22:29:25 +0900

    edit MaxFlowEdmondsKarp

--- src/main/java/com/example/demo/homework/MaxFlowEdmondsKarp.java
+++ src/main/java/com/example/demo/homework/MaxFlowEdmondsKarp.java
@@ -1,12 +1,11 @@
 package com.example.demo.homework;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class MaxFlowEdmondsKarp {
 
     static class Edge {
-        int s, t, rev, cap, flow;
+        int s, t, rev, cap, f;
 
         public Edge(int s, int t, int rev, int cap) {
             this.s = s;
@@ -29,7 +28,7 @@ public class MaxFlowEdmondsKarp {
     }
 
     public static int maxFlow(List<Edge>[] graph, int s, int t) {
-        int maxFlow = 0;
+        int flow = 0;
         int[] q = new int[graph.length];
         while (true) {
             int qt = 0;
@@ -38,7 +37,7 @@ public class MaxFlowEdmondsKarp {
             for (int qh = 0; qh < qt && pred[t] == null; qh++) {
                 int cur = q[qh];
                 for (Edge e : graph[cur]) {
-                    if (pred[e.t] == null && e.cap > e.flow) {
+                    if (pred[e.t] == null && e.cap > e.f) {
                         pred[e.t] = e;
                         q[qt++] = e.t;
                     }
@@ -48,14 +47,14 @@ public class MaxFlowEdmondsKarp {
                 break;
             int df = Integer.MAX_VALUE;
             for (int u = t; u != s; u = pred[u].s)
-                df = Math.min(df, pred[u].cap - pred[u].flow);
+                df = Math.min(df, pred[u].cap - pred[u].f);
             for (int u = t; u != s; u = pred[u].s) {
-                pred[u].flow += df;
-                graph[pred[u].t].get(pred[u].rev).flow -= df;
+                pred[u].f += df;
+                graph[pred[u].t].get(pred[u].rev).f -= df;
             }
-            maxFlow += df;
+            flow += df;
         }
-        return maxFlow;
+        return flow;
     }
 
     // Usage example
@@ -64,7 +63,7 @@ public class MaxFlowEdmondsKarp {
         addEdge(graph, 0, 1, 3);
         addEdge(graph, 0, 2, 2);
         addEdge(graph, 1, 2, 2);
-        System.out.println(4 == maxFlow(graph, 0, 2));
+        System.out.println("The maximum possible flow is " + maxFlow(graph, 0, 2));
 
         List<Edge>[] graph1 = createGraph(5);
         addEdge(graph1, 0, 1, 3);
@@ -72,7 +71,6 @@ public class MaxFlowEdmondsKarp {
         addEdge(graph1, 2, 1, 5);
         addEdge(graph1, 1, 3, 2);
         addEdge(graph1, 2, 3, 2);
-        System.out.println(4 == maxFlow(graph1, 0, 3));
-
+        System.out.println("The maximum possible flow is " + maxFlow(graph1, 0, 3));
     }
 }
